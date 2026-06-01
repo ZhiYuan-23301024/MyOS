086a3cf138ff0bd58a347e58f2a861926cbbd759
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-01 16:31:34 +0800

    给main.rs增加新实现的模块，调用batch子模块进行初始化并批量执行应用程序

--- os/src/main.rs
+++ os/src/main.rs
@@ -62,3 +62,38 @@ pub fn rust_main() -> ! {
     println!("Hello, world!");
     panic!("Shutdown machine!");
 }
+#![no_std]
+#![no_main]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod syscall;
+mod trap;
+mod batch;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    let sbss_ptr = sbss as *const () as usize;
+    let ebss_ptr = ebss as *const () as usize;
+    (sbss_ptr..ebss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
+}
+
