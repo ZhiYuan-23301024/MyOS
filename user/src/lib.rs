b616dcd8f0916b746ec89de3413be0a4d223af75
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-01 07:04:50 +0000

    feat: 添加用户程序启动流程和BSS清零功能

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -8,6 +8,36 @@
 
 #![no_std]
 
+#![feature(linkage)]
+
+#[macro_use]
+pub mod console;
+mod syscall;
+mod lang_items;
+
+fn clear_bss() {
+    extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+    let start_bss_ptr = start_bss as *const () as usize;
+    let end_bss_ptr = end_bss as *const () as usize;
+    (start_bss_ptr..end_bss_ptr).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
 use syscall::*;
 
 /// 向文件描述符写入数据
