c287a7f6c35e07a5acdf7e4d15592757135253fc
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-02 11:06:03 +0800

    修复loader.rs的unsafe问题

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -54,12 +54,12 @@ fn get_base_i(app_id: usize) -> usize {
 }
 
 pub fn get_num_app() -> usize {
-    extern "C" { fn _num_app(); }
+    unsafe extern "C" { fn _num_app(); }
     unsafe { (_num_app as *const () as *const usize).read_volatile() }
 }
 
 pub fn load_apps() {
-    extern "C" { fn _num_app(); }
+    unsafe extern "C" { fn _num_app(); }
     let num_app_ptr = _num_app as *const () as *const usize;
     let num_app = get_num_app();
     let app_start = unsafe {
