74e72d5a2cd08ed2c974c8f12eec86ede750e176
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-02 11:14:46 +0800

    context.rs实现任务上下文管理

---
+++ os/src/task/context.rs
@@ -0,0 +1,18 @@
+// os/src/task/context.rs
+
+#[repr(C)]
+pub struct TaskContext {
+    ra: usize,
+    s: [usize; 12],
+}
+
+impl TaskContext {
+    pub fn goto_restore() -> Self {
+        unsafe extern "C" { fn __restore(); }
+        Self {
+            ra: __restore as *const () as usize,
+            s: [0; 12],
+        }
+    }
+}
+
