e3e91a3b1cf9d4530efaa9fd2b0e63ed529869ef
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-09 19:41:28 +0800

    定义物理地址、虚拟地址、物理页号、虚拟页号

---
+++ os/src/mm/address.rs
@@ -0,0 +1,15 @@
+#[repr(C)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysAddr(pub usize);
+
+#[repr(C)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtAddr(pub usize);
+
+#[repr(C)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct PhysPageNum(pub usize);
+
+#[repr(C)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub struct VirtPageNum(pub usize);
