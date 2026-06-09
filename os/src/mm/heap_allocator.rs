7213d0e870035682d6c4ccc46e0302e18d3152f6
ZhiYuan-23301024 <3100498622@qq.com>
2026-06-09 10:39:37 +0800

    全局动态内存分配器

---
+++ os/src/mm/heap_allocator.rs
@@ -0,0 +1,16 @@
+use buddy_system_allocator::LockedHeap;
+use crate::config::KERNEL_HEAP_SIZE;
+
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+static mut HEAP_SPACE: [u8; KERNEL_HEAP_SIZE] = [0; KERNEL_HEAP_SIZE];
+
+pub fn init_heap() {
+    unsafe {
+        HEAP_ALLOCATOR
+            .lock()
+            .init(core::ptr::addr_of!(HEAP_SPACE) as usize, KERNEL_HEAP_SIZE);
+    }
+}
+
